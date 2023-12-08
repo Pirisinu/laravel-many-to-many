46f6847885158e8cb0ce6240211d8986966e858d
Pirisinu <palazzialessandro2@gmail.com>
2023-12-08 20:17:53 +0100

    [ edit ] technologies->index

--- resources/views/admin/technologies/index.blade.php
+++ resources/views/admin/technologies/index.blade.php
@@ -18,15 +18,12 @@
     @foreach ($technologies as $technology)
     <div class="card w-75 my-5 m-auto text-center box my-shadow">
         <div class="card-header">
-          <h4><strong>technology n°:</strong> {{ $technology->id }}</h4>
+          <h4><strong>{{ $technology->name }}</strong></h4>
         </div>
         <div class="card-body">
-          <h5 class="card-title"><strong>technology:</strong> {{ $technology->name }}</h5>
-          <p class="card-text"><strong>technology description:</strong> {{ $technology->description }}</p>
+          <h5 class="card-title"><strong>Technology description:</strong></h5>
+          <p class="card-text">{{ $technology->description }}</p>
           <a href="{{route('admin.technology.show', $technology)}}" class="btn btn-primary">More info</a>
-        </div>
-        <div class="card-footer text-muted">
-            <strong>technology start date:</strong> {{ $technology->start_date }}
         </div>
       </div>
     @endforeach
